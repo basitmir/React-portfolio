6aecaf8edcbf04a333d634c8b4f00ce31479b380
basitmir <iammirbasit@gmail.com>
2021-03-28 00:10:44 +0530

    about complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import Home from './Home';
 
 const About = () => {
-	return <div>About</div>;
+	useEffect(() => {
+		window.open('https://drive.google.com/file/d/1ed1nRbaDqlWl4KllgKiUv29w7cw0T7_R/view', "_blank",'noopener,noreferrer')
+	}, [])
+	return <Home/>;
 };
 
 export default About;
